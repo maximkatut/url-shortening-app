447ef765d92118b95c0e333e3b6e6930b7ea6931
Max Baravy <maximkatut@gmail.com>
2022-06-20 22:34:27 -0400

    copy text to clipboard function

---
+++ src/utils/copyToClipboard.ts
@@ -0,0 +1,22 @@
+const fallbackCopyTextToClipboard = (text: string) => {
+  try {
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying text command was " + msg);
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
+};
+
+export const copyToClipboard = (text: string) => {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(
+    function () {},
+    function (err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
+};
